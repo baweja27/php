286b86962278de74a0b2e88b27fe5b41e3ab0836
baweja27 <118948485+baweja27@users.noreply.github.com>
2022-11-23 21:45:32 -0500

    Delete Assignment 3 directory

--- Assignment 3/sql/students-records.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE students (
-    id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    stuid varchar(100) NOT NULL,
-    name varchar(100) NOT NULL,
-    email varchar(100) NOT NULL,
-    salary varchar(100) NOT NULL,
-    subject varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
