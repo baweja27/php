36dc194f987f28c826432bfd1452171bad902c9c
baweja27 <118948485+baweja27@users.noreply.github.com>
2022-11-23 21:46:15 -0500

    Add files via upload

---
+++ Assignment 3/sql/profile.sql
@@ -0,0 +1,14 @@
+CREATE TABLE profile (
+    id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    profession varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    username varchar(100) NOT NULL,
+    address varchar (100) NOT NULL,
+    phone varchar(100) not null,
+    mobile varchar(100) not null
+    
+    
+    
+    
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
